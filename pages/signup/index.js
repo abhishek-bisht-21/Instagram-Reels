b8b6906d734633dcdb2d7f2f2ba043d748e21d23
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-15 02:07:17 +0530

    Sign up and Upload button created

--- pages/signup/index.js
+++ pages/signup/index.js
@@ -3,16 +3,31 @@ import TextField from '@mui/material/TextField';
 import Image from 'next/image'
 // In built Image in NextJs
 import insta from '../../assets/insta.jpg'
+import Button from '@mui/material/Button';
 // Routinmg is Handled by NextJS itself
 function index() {
 	return (
 
 		<div className="signup-container">
-			
+
 			<div className="signup-card">
 				<Image src={insta} />
-				<TextField id="outlined-basic" label="Outlined" variant="outlined" /> 
+				<TextField size="small" id="outlined-basic" margin="dense" label="Email" variant="outlined" fullWidth />
+				<TextField size="small" id="outlined-basic" type="password" margin="dense" label="Password" variant="outlined" fullWidth />
+				<TextField size="small" id="outlined-basic" margin="dense" label="Full Name" variant="outlined" fullWidth />
+				<Button variant="outlined" component="label" fullWidth style={{marginTop:'1rem'}}>
+					<input type="file" accept="image/*" style={{display:'none'}}/>
+					Upload
+				</Button>
 
+				<Button variant="contained" component="span" fullWidth style={{marginTop:'1rem'}}>
+					
+					Sign Up
+				</Button>
+
+			</div>
+
+			<div>
 			</div>
 
 		</div>
