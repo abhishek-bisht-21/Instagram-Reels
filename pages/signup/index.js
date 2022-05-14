772630063b0eebad28b8da26fd5d7b79a8f1062d
Abhishek Bisht <abhishek.bisht54@yahoo.com>
2022-05-14 23:32:38 +0530

    Setting up asset folder and Signup page

---
+++ pages/signup/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TextField from '@mui/material/TextField';
+import Image from 'next/image'
+import insta from '../../assets/insta.jpg'
+
+function index() {
+	return (
+
+		<div className="signup-container">
+			
+			<div className="signup-card">
+				<Image src={insta} />
+				<TextField id="outlined-basic" label="Outlined" variant="outlined" /> 
+
+			</div>
+
+		</div>
+	)
+}
+
+export default index
